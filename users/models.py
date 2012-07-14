add342bc15334507d34bbed75945d68c872a5b08
Vinod Kurup <vinod@kurup.com>
2012-07-13 21:05:26 -0400

    Halfway through user modeling
    Left off at 6.3.2

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    password_digest = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return "%s (%s)" % (self.name, self.email)
+
+    def clean(self):
+        self.email = self.email.lower()
--- users/tests.py
+++ users/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.test.client import Client
 from django.core.urlresolvers import reverse
+from django.core.exceptions import ValidationError
+from models import User
 
 class UserPagesTest(TestCase):
     def setUp(self):
@@ -13,3 +15,51 @@ class UserPagesTest(TestCase):
     def test_should_have_right_title(self):
         self.assertContains(self.response, '<title>Tutorial Sample App | Sign up</title>')
 
+class UserTest(TestCase):
+    def setUp(self):
+        self.user = User(name='Example User', email='user@example.com')
+        self.user.save()
+
+    def test_name_present(self):
+        self.assertIsNotNone(self.user.name)
+
+    def test_email_present(self):
+        self.assertIsNotNone(self.user.email)
+
+    def test_password_present(self):
+        self.assertIsNotNone(self.user.password_digest)
+
+    def test_user_valid(self):
+        self.user.full_clean()
+        self.assertTrue(self.user)
+
+    def test_user_invalid_when_name_absent(self):
+        self.user.name = ''
+        self.assertRaisesMessage(ValidationError, '', self.user.full_clean)
+
+    def test_user_invalid_when_email_absent(self):
+        self.user.email = ''
+        self.assertRaisesMessage(ValidationError, '', self.user.full_clean)
+
+    def test_user_invald_when_name_too_long(self):
+        self.user.name = 'a' * 51
+        self.assertRaisesMessage(ValidationError, '', self.user.full_clean)
+        
+    def test_user_invalid_when_email_invalid(self):
+        addresses = 'user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com'.split()
+        for email in addresses:
+            self.user.email = email
+            self.assertRaisesMessage(ValidationError, 'Enter a valid e-mail address', self.user.full_clean)
+        
+    def test_user_valid_when_email_valid(self):
+        addresses = 'user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn'.split()
+        for email in addresses:
+            self.user.email = email
+            self.user.full_clean()
+            self.assertTrue(self.user)
+
+    def test_user_invalid_when_email_taken(self):
+        self.user.id = None
+        user_with_same_email = self.user
+        user_with_same_email.email = self.user.email.upper()
+        self.assertRaisesMessage(ValidationError, '', user_with_same_email.full_clean)
